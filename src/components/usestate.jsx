82d20e81d848068f50583fc5e959887c73596280
Nirman529 <68377229+Nirman529@users.noreply.github.com>
2023-07-09 15:12:22 +0530

    added description to the usestate hook

--- src/components/usestate.jsx
+++ src/components/usestate.jsx
@@ -14,9 +14,12 @@ const Usestate = () => {
     return (
         <div className='justify-content-center text-center'>
             <h3>UseState Demo</h3>
+            <p>Here in the use state hook which utilises two instances a value and a function initialised with a certain value which can be anything from an intege to an array of objects</p>
+            <p>syntax is: const [count, setCount] = useState(0)</p>
+            <p>Here count is the variable and the setCount is the function initialised with the value of 0</p>
             <div className='m-1'>{count}</div>
-            <button className='btn btn-danger' onClick={() => { decrement() }}>Subtract</button>
-            <button className='btn btn-primary' onClick={() => { increment() }}>Add</button>
+            <button className='btn btn-danger m-2' onClick={() => { decrement() }}>Subtract</button>
+            <button className='btn btn-primary m-2' onClick={() => { increment() }}>Add</button>
         </div>
     )
 }
